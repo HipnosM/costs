09ad5599384424ba67e115268b5d2d644659fe8c
HipnosM <iagoandre567@gmail.com>
2023-12-12 22:02:22 -0300

    refeito o menu de navegação, feita com menu sanduiches e animação

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,22 +1,39 @@
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
 import Container from "./Container";
 
 import Logo from "../../assets/costs_logo.png";
 import styles from "./Navbar.module.css";
+import { useState } from "react";
 
 export default function Navbar() {
+
+    const [menu, setMenu] = useState(false);
+    const [menuAberto, setMenuToggle] = useState(true);
+
+    function menuToggle() {
+        setMenu(!menu);
+        setMenuToggle(!menuAberto);
+    }
+
     return (
         <nav className={styles.navbar}>
             <Container>
                 <Link to="/">
                     <img src={Logo} alt="logo costs" />
                 </Link>
-                <ul className={styles.list}>
-                    <li className={styles.item}><Link to="/">Home</Link></li>
-                    <li className={styles.item}><Link to="/projetos">Projetos</Link></li>
-                    <li className={styles.item}><Link to="/empresa">Empresa</Link></li>
-                    <li className={styles.item}><Link to="/contato">Contato</Link></li>
-                </ul>
+                <div className={styles.list}>
+                    <ul className={`${styles.list} ${menu ? styles.abrir : ""}`}>
+                        <li className={styles.item}><Link to="/">Home</Link></li>
+                        <li className={styles.item}><Link to="/projetos">Projetos</Link></li>
+                        <li className={styles.item}><Link to="/empresa">Empresa</Link></li>
+                        <li className={styles.item}><Link to="/contato">Contato</Link></li>
+                    </ul>
+                    <div className={styles.menu}>
+                        <FaBars onClick={menuToggle} className={menuAberto ? styles.menuAberto : ""} />
+                    </div>
+                </div>
             </Container>
         </nav>
     );
